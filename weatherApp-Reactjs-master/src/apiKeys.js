9ee313d18081b73b799ba8466fd1f9c0c37a9e98
Likitha <bankalikitha.20.csd@anits.edu.in>
2023-11-19 09:38:49 +0530

    hello

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b6aaf74567cf2ca1773092674aaafc18",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
